ce2456d9d2089510ac9006dace3a013ee0dae51e
nsv <rohanlaidlaw092@gmail.com>
2018-03-29 21:09:34 +0200

    Prefix Index Number

--- nwn.sh
+++ nwn.sh
@@ -4,12 +4,12 @@ now=$(date +"%m_%d_%Y__%H:%M:%S")
 nwndir=/path/linux-x86;
 nwnlogsdir=/path/.local/share/'Neverwinter Nights'/logs
 nwnserver=""
-
 servernames=$(curl -sS https://api.nwn.beamdog.net/v1/servers )
-echo $servernames | jq -r -C '. | sort_by(.current_players) | .[] | "\(.id). -- \(.session_name) | Players: \(.current_players) / \(.max_players)"'
+
+echo $servernames | jq -r -C '. | sort_by(.current_players) | .[] | "\(.session_name) | Players: \(.current_players) / \(.max_players)"' | nl
+
 echo "Pick a Server:"
 read serverpick
-
 address=`echo $servernames | jq -r --argjson serverpick $serverpick '. | .[$serverpick] | "\(.host):\(.port)"'`
 
 pushd "$nwndir"
