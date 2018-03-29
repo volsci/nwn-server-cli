69d2c0f8f9fd5c346dcb0904155b199810865c8d
nsv <rohanlaidlaw092@gmail.com>
2018-03-29 18:47:34 +0200

    Implement Server Selection

--- nwn.sh
+++ nwn.sh
@@ -1,10 +1,16 @@
+#!/bin/bash
+
 now=$(date +"%m_%d_%Y__%H:%M:%S")
 nwndir=/<YOUR PATH>/linux-x86;
 nwnlogsdir=/<YOUR PATH>/'Neverwinter Nights'/logs
 nwnserver=""
 
 servernames=$(curl -sS https://api.nwn.beamdog.net/v1/servers)
-echo $servernames | jq -r '. | sort_by(.current_players) | .[] | "\(.session_name) \(.current_players)"'
+echo $servernames | jq -r -C '. | sort_by(.current_players) | .[] | "\(.session_name) | Players: \(.current_players) / \(.max_players)"'
+echo "Pick a Server:"
+read serverpick
+
+echo $servernames | jq -r --argjson serverpick $serverpick '. | .[$serverpick] | "\(.host):\(.port)"'
 
 # pushd "$nwndir"
 # ./nwmain-linux +connect "$nwnserver" "$@"
