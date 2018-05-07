fc8fb406544596b85ebd0a5454f0a505e7ce4d82
nsv <rohanlaidlaw092@gmail.com>
2018-05-07 13:16:46 +0200

    Fix Indexing Discrepancy

--- nwn.sh
+++ nwn.sh
@@ -6,7 +6,7 @@ nwnlogsdir=/path/.local/share/'Neverwinter Nights'/logs
 nwnserver=""
 servernames=$(curl -sS https://api.nwn.beamdog.net/v1/servers )
 
-echo $servernames | jq -r -C '. | sort_by(.current_players) | .[] | "\(.session_name) | Players: \(.current_players) / \(.max_players)"' | nl
+echo $servernames | jq -r -C '. | sort_by(.current_players) | .[] | "\(.session_name) | Players: \(.current_players) / \(.max_players)"' | nl --starting-line-number=0
 
 echo "Pick a Server:"
 read serverpick
