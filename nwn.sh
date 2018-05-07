8c3173c75bdc1776c1d75da8e95cd2f7ca01db43
nsv <rohanlaidlaw092@gmail.com>
2018-05-07 17:08:08 +0200

    Implement Hacky Directory Finder And Fix JQ Checker

--- nwn.sh
+++ nwn.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 now=$(date +"%m_%d_%Y__%H:%M:%S")
-nwndir=/path/linux-x86;
-nwnlogsdir=/path/.local/share/'Neverwinter Nights'/logs
-servernames=$(curl -sS https://api.nwn.beamdog.net/v1/servers )
-
+nwndir=$(find . -name "nwmain-linux" -exec dirname {} \;);
+nwnlogsdir=$(find . -name "nwclientLog1.txt" -exec dirname {} \;);
 address=""
 
+echo $nwndir
+echo $nwnlogsdir
+
+# Check that jq is installed
+if ! [ -x "$(command -v jq)" ]; then
+  echo 'Error: git is not installed.' >&2
+  exit 1
+fi
+
+# Check for the existence of an argument, and if one exists, direct connect
+# and bypass the serverlist
 if [ $# -eq 0 ]
     then
+        servernames=$(curl -sS https://api.nwn.beamdog.net/v1/servers )
         echo $servernames | jq -r -C '. | sort_by(.current_players) | .[] | "\(.session_name) | Players: \(.current_players) / \(.max_players)"' | nl --starting-line-number=0
         echo "Pick a Server:"
         read serverpick
