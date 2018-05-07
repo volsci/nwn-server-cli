ec06c92897a67a16c89ca181029d7e74d762ff86
nsv <rohanlaidlaw092@gmail.com>
2018-05-07 13:35:00 +0200

    Pass Server as Arg

--- nwn.sh
+++ nwn.sh
@@ -5,11 +5,17 @@ nwndir=/path/linux-x86;
 nwnlogsdir=/path/.local/share/'Neverwinter Nights'/logs
 servernames=$(curl -sS https://api.nwn.beamdog.net/v1/servers )
 
-echo $servernames | jq -r -C '. | sort_by(.current_players) | .[] | "\(.session_name) | Players: \(.current_players) / \(.max_players)"' | nl --starting-line-number=0
+address=""
 
-echo "Pick a Server:"
-read serverpick
-address=`echo $servernames | jq -r --argjson serverpick $serverpick '. | .[$serverpick] | "\(.host):\(.port)"'`
+if [ $# -eq 0 ]
+    then
+        echo $servernames | jq -r -C '. | sort_by(.current_players) | .[] | "\(.session_name) | Players: \(.current_players) / \(.max_players)"' | nl --starting-line-number=0
+        echo "Pick a Server:"
+        read serverpick
+        address=`echo $servernames | jq -r --argjson serverpick $serverpick '. | .[$serverpick] | "\(.host):\(.port)"'`
+    else
+        address=$1
+fi
 
 pushd "$nwndir"
     ./nwmain-linux +connect "$address" "$@"
